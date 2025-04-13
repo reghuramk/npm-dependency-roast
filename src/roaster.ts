5946ccc4c244c3576c6ad6ae0dcba85548ffbaaa
reghuramk <reghuramk04@gmail.com>
2025-04-13 22:34:38 +0100

    sample roasts

--- src/roaster.ts
+++ src/roaster.ts
@@ -3,7 +3,28 @@ import chalk from "chalk";
 import { PackageJson } from "./types";
 
 const roastDependencies = (pkg: PackageJson): void => {
-
+    const deps = pkg.dependencies || {};
+    const devDeps = pkg.devDependencies || {};
+  
+    console.log(chalk.bold(`Roasting dependencies in ${pkg.name || 'your project'}:`));
+  
+    if ('lodash' in deps && 'underscore' in deps) {
+      console.log(chalk.yellow("You’re using both lodash and underscore? That's like using two spoons for cereal."));
+    }
+  
+    if ('moment' in deps) {
+      console.log(chalk.magenta("Still using moment? It's 2025. Consider date-fns or dayjs."));
+    }
+  
+    if (Object.keys(devDeps).length > Object.keys(deps).length) {
+      console.log(chalk.cyan("Your devDependencies outweigh your dependencies. Building an app or a lab experiment?"));
+    }
+  
+    if ('request' in deps) {
+      console.log(chalk.red("request is dead. Use axios or fetch like a modern dev."));
+    }
+  
+    console.log(chalk.green("\nRoast complete. No feelings were harmed... hopefully."));
 }
 
 export default roastDependencies
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export interface PackageJson {
+  name: string;
   dependencies: Record<string, string>;
   devDependencies: Record<string, string>;
   peerDependencies: Record<string, string>;
